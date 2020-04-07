66f5ed11ffaf01ba18e285ca05f1e2cdc478df43
Liam Anthony Edwards <liam0531@stud.kea.dk>
2020-04-07 20:28:01 +0200

    Sorted out my javaScript burger menu so it looks clean on any device

---
+++ javaScript.js
@@ -0,0 +1,10 @@
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '100%';
+
+    document.getElementById('main').style.marginRight = '100%';
+}
+
+function closeSlideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('main').style.marginRight = '0';
+}
